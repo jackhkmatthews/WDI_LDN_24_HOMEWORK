526300b896f7c939fefbc3d1a1c349f526335538
jackhkmatthews <jackhkmatthews@gmail.com>
2016-11-30 18:34:55 +0000

    added more comments to code

--- jackhkmatthews/w01d03/calculator/js/app.js
+++ jackhkmatthews/w01d03/calculator/js/app.js
@@ -1,26 +1,27 @@
 //welcome user
 var again = prompt('Hello user.  Please type "on" to use the calculator');
 
-//start loop
+//start calculator prompt and alert loop
 while (again.toUpperCase() === 'YES' || again.toUpperCase() === 'ON' ){
 
-  //define operator
+  //define users chosen operator in order to allow toUpperCase() of 'sqrt' and to reset at the start of a new loop
   var operator = 'operator';
 
-  //get kind of operation would the user like to use
+  //get kind of operation the user would like to use
+  //prompt repeatedly until one of available operators selected
   while (operator !== '*' && operator !== '/' && operator !== '+' && operator !== '-' && operator !== '^' && operator.toUpperCase() !== 'SQRT'){
     operator = prompt('Please type *, /, +, -, ^ or SQRT to select one as your calculation operator');
   }
 
-  //get users first number
+  //get user's first number
   var firstNumber = parseFloat(prompt('Please enter your first number'));
 
-  //get users second number if required
+  //get user's second number if required (not required if sqrt selected as operator)
   if (operator.toUpperCase() !== 'SQRT') {
     var secondNumber = parseFloat(prompt('Please enter your second number'));
   }
 
-  //calculate answer
+  //calculate answer via switch case statement based on user submitted operator
   var answer;
   switch (operator.toUpperCase()) {
     case '*':
@@ -45,13 +46,14 @@ while (again.toUpperCase() === 'YES' || again.toUpperCase() === 'ON' ){
       break;
   }
 
-  //display answer to user depending on selceted operator
+  //display answer string to user depending on selected operator
   if (operator.toUpperCase() !== 'SQRT') {
     alert(firstNumber + ' ' + operator + ' ' + secondNumber + ' = ' + answer);
   } else {
     alert(operator + ' of ' + firstNumber + ' = ' + answer);
   }
 
-  //ask user if another calculation is required and exit loop if not required
+  //ask user if another calculation is required and exit loop if not
+  //loop exited by inital while loop condition if anything by yes is entered
   again = prompt('Type "yes" to use the calculator again.');
 }
