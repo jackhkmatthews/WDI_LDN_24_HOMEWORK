039b0fae9c05e8cb5901a20957b5b359b2f6f9d3
jackhkmatthews <jackhkmatthews@gmail.com>
2016-12-02 17:01:18 +0000

    not sure

--- jackhkmatthews/w01d04/calculator/js/app.js
+++ jackhkmatthews/w01d04/calculator/js/app.js
@@ -1,11 +1,12 @@
-//declaring value getting function
+//run app when page loaded
 
-function getElementValueByID(id, parse) {
-  if (parse){
-    return parseFloat(document.getElementById(id).value);
-  } else {
-    return document.getElementById(id).value;
-  }
+window.onload = app;
+
+//declare app function which will find form and make it listen
+
+function app() {
+  //get form element and make submit listen
+  document.getElementById('calculatorForm').addEventListener('submit', formSubmit);
 }
 
 //declaring function to be added to form listen on submit
@@ -14,43 +15,60 @@ function formSubmit(e) {
   //remove default behavior
   e.preventDefault();
   //get firstNumber, operator and secondNumber values
-  var firstNumber = getElementValueByID('firstNumber', true);
-  var operator = getElementValueByID('operator', false);
-  var secondNumber = getElementValueByID('secondNumber', true);
-  //calculate answer
-  var answer;
+  var firstNumber  = getElementValueByID('firstNumber', 'parse');
+  var operator     = getElementValueByID('operator');
+  var secondNumber = getElementValueByID('secondNumber', 'parse');
+  //calculate answer and print
   switch (operator) {
     case '*':
-      answer = firstNumber * secondNumber;
+      print(multiply(firstNumber, secondNumber));
       break;
     case '-':
-      answer = firstNumber - secondNumber;
+      print(subtract(firstNumber, secondNumber));
       break;
     case '/':
-      answer = firstNumber / secondNumber;
+      print(divide(firstNumber, secondNumber));
       break;
     case '+':
-      answer = firstNumber + secondNumber;
+      print(add(firstNumber, secondNumber));
       break;
     default:
       break;
   }
-  //print answer to output
+  // //print answer to output
+  // var output = document.getElementById('output');
+  // output.innerHTML = answer;
+  // //log form submitted
+  // console.log('Form submitted');
+}
+
+//declaring value getting function
+
+function getElementValueByID(id, parse) {
+  if (parse === 'parse'){
+    return parseFloat(document.getElementById(id).value);
+  } else {
+    return document.getElementById(id).value;
+  }
+}
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function print (answer) {
   var output = document.getElementById('output');
   output.innerHTML = answer;
-  //log form submitted
-  console.log('Form submitted');
 }
-
-//declare app function which will find form and make it listen
-
-function app() {
-  //get form element
-  var form = document.getElementById('calculatorForm');
-  //make submit listen
-  form.addEventListener('submit', formSubmit);
-}
-
-//run app when page loaded
-
-window.onload = app;
