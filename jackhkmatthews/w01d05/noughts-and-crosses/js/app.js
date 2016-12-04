a4727447b1dd0a27ca7a84d646a76f49ee0ce6b5
jackhkmatthews <jackhkmatthews@gmail.com>
2016-12-04 15:49:29 +0000

    refactored win check to include array checking.  console log winner is working.  flashing alert removed for now

--- jackhkmatthews/w01d05/noughts-and-crosses/js/app.js
+++ jackhkmatthews/w01d05/noughts-and-crosses/js/app.js
@@ -4,8 +4,30 @@ console.log('js loaded');
 //////////////noughts and crosses///////////////
 ////////////////////////////////////////////////
 
+//the board
+// box0 box1 box2
+// box3 box4 box5
+// box6 box7 box8
+
 //get all boxes
 var boxes = document.getElementsByClassName('box');
+
+//create players to allow storage of 'moves' i.e which lis are in thier name
+var playerXBoxes =[];
+var playerNaughtBoxes = [];
+
+//wining combinations of boxes
+//if a player 'controls' these boxes they have won
+var winningBoxCombinations = [
+                          ['box0', 'box1', 'box2'], //rows
+                          ['box3', 'box4', 'box5'],
+                          ['box6', 'box7', 'box8'],
+                          ['box0', 'box3', 'box6'], //columns
+                          ['box1', 'box4', 'box7'],
+                          ['box2', 'box5', 'box8'],
+                          ['box0', 'box4', 'box8'], //diagonals
+                          ['box2', 'box4', 'box6']
+];
 
 //wait for doc to load
 window.onload = app;
@@ -21,23 +43,28 @@ function app() {
   var turnIndicator = document.getElementById('turnIndicator');
   turnIndicator.innerHTML = 'x is next';
 
-  //when li clicked should mark box as black or white depeding on whose turn it is, the same cell cannot be clicked twice before cleared
+  //when li clicked should mark box as black or white depeding on whose turn it is, the same cell cannot be clicked twice before cleared. should also check for winner
 
   //make each li listen for clicks with for loop
   for (var i = 0; i < boxes.length; i ++) {
     boxes[i].addEventListener('click', function(){
-      //if innerHTML is empty/false
-      if(this.innerHTML === '.'){
+      //if li has already been clicked (class includes 'clicked')
+      var clicked = this.className.includes('clicked');
+      if(!clicked){
         //if xIsNext variable is true
         if(xIsNext){
-          //set li inner html to x and add class background white and color black
+          //set li inner html to x, add class background white and color black
+          //and store playerX's move in playerXBoxes
           this.innerHTML = 'x';
           this.className += ' x';
+          playerXBoxes.push(this.id);
         //else
         } else {
-          //set li inner html to 0 and add class background black and color white
+          //set li inner html to 0, add class background black and color white
+          //and store playerNaught's move in playerNaughtBoxes
           this.innerHTML = '0';
           this.className += ' naught';
+          playerNaughtBoxes.push(this.id);
         }
         //reverse xIsNextVariable
         xIsNext = !xIsNext;
@@ -45,15 +72,18 @@ function app() {
         if (xIsNext){
           //set turn indicator innerhtml to "X is next"
           turnIndicator.innerHTML = 'x is next';
+          //test if 0 has won
+          checkForWinner(playerNaughtBoxes);
         //else
         } else {
           //set turn indicator innerhtml to "0 is next"
           turnIndicator.innerHTML = '0 is next';
+          //check if x has won
+          checkForWinner(playerXBoxes);
         }
-      //else
-        //do nothing
-      }
-      checkAndAlertAndUnbind()
+        //add clicked class to li
+        this.className += ' clicked';
+      } //else do nothing
     });
   }
 
@@ -71,75 +101,31 @@ function app() {
   });
 }
 
-//alert the user if there is a winner by  flashing winning boxes
-//function to check if winning combination exists
-function checkAndAlertAndUnbind(){
-  //create isWinner variable and set to false
-  var isWinner = false;
-  //create an empty array of boxinnerHTML
-  var boxesInnerHtml = [];
-  //populate with innerHTML
-  for (var i = 0; i < boxes.length; i ++) {
-    boxesInnerHtml.push(boxes[i].innerHTML);
-  }
-  //winning conditions switch case if one is true then flash boxes
-  switch (true) {
-    //rows
-    case boxesInnerHtml[0] === boxesInnerHtml[1] && boxesInnerHtml[0] === boxesInnerHtml[2] && boxesInnerHtml[0] !== '.':
-      console.log('winner');
-      boxes[0].className += ' flashing';
-      boxes[1].className += ' flashing';
-      boxes[2].className += ' flashing';
-      break;
-    case boxesInnerHtml[3] === boxesInnerHtml[4] && boxesInnerHtml[3] === boxesInnerHtml[5] && boxesInnerHtml[3] !== '.':
-      console.log('winner');
-      boxes[3].className += ' flashing';
-      boxes[4].className += ' flashing';
-      boxes[5].className += ' flashing';
-      break;
-    case boxesInnerHtml[6] === boxesInnerHtml[7] && boxesInnerHtml[6] === boxesInnerHtml[8] && boxesInnerHtml[6] !== '.':
-      console.log('winner');
-      boxes[6].className += ' flashing';
-      boxes[7].className += ' flashing';
-      boxes[8].className += ' flashing';
-      break;
-    //columns
-    case boxesInnerHtml[0] === boxesInnerHtml[3] && boxesInnerHtml[0] === boxesInnerHtml[6] && boxesInnerHtml[0] !== '.':
-      console.log('winner');
-      boxes[0].className += ' flashing';
-      boxes[3].className += ' flashing';
-      boxes[6].className += ' flashing';
-      break;
-    case boxesInnerHtml[1] === boxesInnerHtml[4] && boxesInnerHtml[1] === boxesInnerHtml[7] && boxesInnerHtml[1] !== '.':
-      console.log('winner');
-      boxes[1].className += ' flashing';
-      boxes[4].className += ' flashing';
-      boxes[7].className += ' flashing';
-      break;
-    case boxesInnerHtml[2] === boxesInnerHtml[5] && boxesInnerHtml[2] === boxesInnerHtml[8] && boxesInnerHtml[2] !== '.':
-      console.log('winner');
-      boxes[2].className += ' flashing';
-      boxes[5].className += ' flashing';
-      boxes[8].className += ' flashing';
-      break;
-    //diagonals
-    case boxesInnerHtml[0] === boxesInnerHtml[4] && boxesInnerHtml[0] === boxesInnerHtml[8] && boxesInnerHtml[0] !== '.':
-      console.log('winner');
-      boxes[0].className += ' flashing';
-      boxes[4].className += ' flashing';
-      boxes[8].className += ' flashing';
-      break;
-    case boxesInnerHtml[2] === boxesInnerHtml[4] && boxesInnerHtml[2] === boxesInnerHtml[6] && boxesInnerHtml[2] !== '.':
-      console.log('winner');
-      boxes[2].className += ' flashing';
-      boxes[4].className += ' flashing';
-      boxes[6].className += ' flashing';
-      break;
-    default:
-      break;
-  }
-  for (var j = 0; j < boxes.length; j ++) {
-    boxes[j].addEventListener('click', function(){
-    });
+//function to check each players contorled boxes for a winning combination
+function checkForWinner (playersControledBoxes) {
+  //get each array from the winnig combination array of arrays
+  for (var i = 0; i < winningBoxCombinations.length; i ++){
+    console.log('entered first for loop');
+    //get each element from that nested array (e.g each box ID in one of the row winning conditios)
+    var winningBoxCombination = winningBoxCombinations[i];
+    console.log(winningBoxCombination);
+    //declare controlledWinningBoxes
+    var controlledWinningBoxes = 0;
+    for (var j = 0; j < winningBoxCombination.length; j ++){
+      console.log('entered second for loop');
+      //test if each element in turn is included in the players controlled boxes array
+      if (playersControledBoxes.includes(winningBoxCombination[j])){
+        //if included, add 1 to controlledWinningBoxes
+        controlledWinningBoxes += 1;
+        console.log('controlledWinningBoxes: ' + controlledWinningBoxes);
+        //if three elements have been found we have a winner, console log
+        if (controlledWinningBoxes === 3){
+          console.log('winner');
+        } else {
+          console.log('no winner');
+          console.log('controlled winning boxes: ' + controlledWinningBoxes);
+        }
+      }
+    }
   }
 }
