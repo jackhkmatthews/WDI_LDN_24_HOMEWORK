d39b13ed2afb27f680f42ee7f20cb88ea6322725
jackhkmatthews <jackhkmatthews@gmail.com>
2016-12-08 23:01:43 +0000

    read me for code karaoke

---
+++ jackhkmatthews/w02d04/game-project/code-lyrics.js
@@ -0,0 +1,46 @@
+// Daylight
+// I wake up feeling like you won't play right
+// I used to know, but now that shit don't feel right
+// It made me put away my pride
+// So long
+// You made a nigga wait for some, so long
+// You make it hard for a boy like that to know wrong
+// I'm wishing I could make this mine, oh
+
+
+
+var Daylight = {
+  wakeUp: function wakeUp(I){
+    return !playRight;
+  }
+};
+
+var I = 'used to know';
+var thatShit = !feelRight;
+
+function it(){
+  me.put('../away', function (pride, res) {
+    var bad = pride.feeling;
+  });
+};
+
+setTimeout(soLong(), 200000);
+
+$('nigga').addEventListener('click', function(){
+  setTimeout(function(){
+    return SOME
+  }, 60000);
+});l
+
+var boy.doWrong;
+if (haveClimedEverest){
+  boy.doWrong = true;
+} else {
+  boy.doWrong = false
+};
+
+var im = {
+  wishing: function(){
+    mine = this;
+  }
+};
