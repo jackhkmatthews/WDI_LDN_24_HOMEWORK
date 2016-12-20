d35f58407ef5ab9fcef7571749b8fd62a7e56c64
jackhkmatthews <jackhkmatthews@gmail.com>
2016-12-20 11:04:05 +0000

    fleshing out the site

--- jackhkmatthews/w04d01/bootstrap/config/router.js
+++ jackhkmatthews/w04d01/bootstrap/config/router.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('index', {title: 'Home'}));
-router.get('/work', (req, res) => res.render('work', {title: 'Work'}));
+router.get('/projects', (req, res) => res.render('index', {title: 'Work'}));
 router.get('/about', (req, res) => res.render('about', {title: 'About'}));
 router.get('/contact', (req, res) => res.render('contact', {title: 'Contact'}));
-router.get('/project', (req, res) => res.render('project', {title: 'Project'}));
-
+router.get('/projects/:project', (req, res) => {
+  const project = req.params.project;
+  return res.render(`projects/${project}`, {title: 'Projects', layout: 'project-layout' });
+});
 module.exports.router = router;
