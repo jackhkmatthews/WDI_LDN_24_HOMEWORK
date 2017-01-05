87de05300e5f9f7b11f4fb3c54e99c36d8fb6153
jackhkmatthews <jackhkmatthews@gmail.com>
2017-01-05 14:12:44 +0000

    dried code a little

--- jackhkmatthews/w05d03/starter-code/client/src/js/app.js
+++ jackhkmatthews/w05d03/starter-code/client/src/js/app.js
@@ -1,24 +1,29 @@
 console.log('js has loaded');
-const API = 'http://localhost:4000';
 
 $(init);
+
+const API = 'http://localhost:4000';
 
 function init() {
   usersIndex();
   $('.usersIndex').on('click', usersIndex);
+
+  $('.usersNew').on('click', usersNew);
+  $('body').on('submit', '.usersCreate', usersCreate);
   $('body').on('click', '.usersShow', usersShow);
   $('body').on('click', '.usersDelete', usersDelete);
   $('body').on('click', '.usersEdit', usersEdit);
   $('body').on('submit', '.usersUpdate', usersUpdate);
-  $('.usersNew').on('click', usersNew);
-  $('body').on('submit', '.usersCreate', usersCreate);
+
   $('body').on('click', '.projectsNew', projectsNew);
   $('body').on('submit', '.projectsCreate', projectsCreate);
+  $('body').on('click', '.projectsDelete', projectsDelete);
   $('body').on('click', '.projectsEdit', projectsEdit);
   $('body').on('submit', '.projectsUpdate', projectsUpdate);
-  $('body').on('click', '.projectsDelete', projectsDelete);
 }
 
+
+//////////users/////////////
 function usersIndex() {
   $.ajax({
     url: `${API}/users`,
@@ -26,20 +31,62 @@ function usersIndex() {
   }).done(data => {
     $('main').html('<div class="users"></div>');
     $.each(data, (index, user) => {
-      $('.users').prepend(`
-        <div class="col-xs-12 col-sm-4 project-box">
-          <div class="thumbnail">
-            <a href="/users/${user._id}" class="usersShow">
-              <img src="${user.image}" style="max-height: 200px">
-            </a>
-            <div class="caption">
-              <h3 class="title">${user.name}</h3>
-              <p>${user.bio}</p>
-            </div>
-          </div>
-        </div>
-    `);
+      addUser('.users', user);
     });
+  });
+}
+
+function usersNew(){
+  $('.modal-content').html(`
+    <form method="post" action="${API}/users" class="usersCreate">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Add User</h4>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+          <label for="user_name">Name</label>
+          <input class="form-control" type="text" name="user[name]" id="user_name" placeholder="Name">
+        </div>
+        <div class="form-group">
+          <label for="user_image">Image</label>
+          <input class="form-control" type="text" name="user[image]" id="user_image" placeholder="Image">
+        </div>
+        <div class="form-group">
+          <label for="user_github">Github</label>
+          <input class="form-control" type="text" name="user[github]" id="user_github" placeholder="Github">
+        </div>
+        <div class="form-group">
+          <label for="user_bio">Bio</label>
+          <textarea class="form-control" name="user[bio]" id="user_bio" placeholder="Bio"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="user_website">Website</label>
+          <input class="form-control" type="text" name="user[website]" id="user_website" placeholder="Website">
+        </div>
+        <div class="form-group">
+          <label for="user_twitter">Twitter</label>
+          <input class="form-control" type="text" name="user[twitter]" id="user_twitter" placeholder="Twitter">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+    </form>`);
+
+  $('.modal').modal('show');
+}
+
+function usersCreate(e){
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('action'),
+    type: $(this).attr('method'),
+    data: $(this).serialize()
+  }).done(user => {
+    addUser('.users', user);
+    $('.modal').modal('hide');
   });
 }
 
@@ -50,40 +97,7 @@ function usersShow(e){
     type: 'get'
   }).done(user => {
     $('main').html('<div class="users"></div>');
-    $('.users').prepend(`
-    <div class='user-tile'>
-      <img id="userImage" src='${user.image}'>
-      <a class='usersShow' href="/users/${user._id}">
-        <h2 id="userName">${user.name}</h2>
-      </a>
-      <p id="userBio">${user.bio}</p>
-      <p id="userGithub">${user.github}</p>
-      <p id="userTwitter">${user.twitter}</p>
-      <p id="userWebsite">${user.website}</p>
-    </div>
-    <div class="btn-group" role="group" aria-label="...">
-      <button type="button" class="btn btn-default usersEdit" data-id=${user._id}>Edit</button>
-      <button type="button" class="btn btn-danger usersDelete" data-id=${user._id}>Delete</button>
-    </div>
-    <h1>projects</h1>
-    `);
-    $('.users').append(`
-      <button type="button" class="btn btn-default projectsNew" data-id=${user._id}>New</button>
-    `);
-    $(user.projects).each((index, project) =>{
-      $('.users').append(`
-        <div class="project" id="project${index}">
-          <h2 class="title">${project.title}</h2>
-          <p class="description">${project.description}</p>
-          <p class="github">${project.github}</p>
-          <p class="website">${project.website}</p>
-          <div class="btn-group" role="group" aria-label="...">
-            <button type="button" class="btn btn-default projectsEdit" data-project="${project._id}" data-user="${user._id}" data-index="${index}">Edit</button>
-            <button type="button" class="btn btn-danger projectsDelete" data-project="${project._id}" data-user="${user._id}" data-index="${index}" href="/users/${user._id}">Delete</button>
-          </div>
-        </div>
-      `);
-    });
+    addFullUser(user);
   });
 }
 
@@ -98,7 +112,6 @@ function usersDelete(e){
 }
 
 function usersEdit(){
-  // const index = $(e.target).attr('data-project');
   const userName = $('#userName').text();
   const userImage = $('#userImage').attr('src');
   const userGithub = $('#userGithub').text();
@@ -159,69 +172,7 @@ function usersUpdate(e){
   });
 }
 
-function usersNew(){
-  $('.modal-content').html(`
-    <form method="post" action="${API}/users" class="usersCreate">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Add User</h4>
-      </div>
-      <div class="modal-body">
-        <div class="form-group">
-          <label for="user_name">Name</label>
-          <input class="form-control" type="text" name="user[name]" id="user_name" placeholder="Name">
-        </div>
-        <div class="form-group">
-          <label for="user_image">Image</label>
-          <input class="form-control" type="text" name="user[image]" id="user_image" placeholder="Image">
-        </div>
-        <div class="form-group">
-          <label for="user_github">Github</label>
-          <input class="form-control" type="text" name="user[github]" id="user_github" placeholder="Github">
-        </div>
-        <div class="form-group">
-          <label for="user_bio">Bio</label>
-          <textarea class="form-control" name="user[bio]" id="user_bio" placeholder="Bio"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="user_website">Website</label>
-          <input class="form-control" type="text" name="user[website]" id="user_website" placeholder="Website">
-        </div>
-        <div class="form-group">
-          <label for="user_twitter">Twitter</label>
-          <input class="form-control" type="text" name="user[twitter]" id="user_twitter" placeholder="Twitter">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save</button>
-      </div>
-    </form>`);
-
-  $('.modal').modal('show');
-}
-
-function usersCreate(e){
-  e.preventDefault();
-  $.ajax({
-    url: $(this).attr('action'),
-    type: $(this).attr('method'),
-    data: $(this).serialize()
-  }).done(user => {
-
-    $('.users').prepend(`
-    <div class='user-tile'>
-      <img src='${user.image}'>
-      <a class='usersShow' href="/users/${user._id}">
-        <h2>${user.name}</h2>
-      </a>
-      <p>${user.bio}</p>
-    </div>`);
-
-    $('.modal').modal('hide');
-  });
-}
-
+//////////projects/////////////
 function projectsNew(){
   $('.modal-content').html(`
     <form method="post" action="${API}/users/${$(this).attr('data-id')}/projects" class="projectsCreate">
@@ -263,24 +214,22 @@ function projectsCreate(e){
     data: $(this).serialize()
   }).done(project => {
     var index = 0;
-    if (!$('.project:last')) {
-      index = $('.project:last').attr('id').split('t')[1];
+    if ($('.project')) {
+      index = parseInt($('.project:first').attr('id').split('t')[1]) + 1;
     }
     const userId = $('.usersShow').attr('href').split('/')[2];
-    $('.users').append(`
-      <div class="project" id="project${index}">
-        <h2 class="title">${project.title}</h2>
-        <p class="description">${project.description}</p>
-        <p class="github">${project.github}</p>
-        <p class="website">${project.website}</p>
-        <div class="btn-group" role="group" aria-label="...">
-          <button type="button" class="btn btn-default projectsEdit" data-project="${project._id}" data-user="${userId}" data-index="${index}">Edit</button>
-          <button type="button" class="btn btn-danger projectsDelete" data-project="${project._id}" data-user="${userId}" data-index="${index}" href="/users/${userId}">Delete</button>
-        </div>
-      </div>
-    `);
-
+    addProject('.projects', userId, project, index);
     $('.modal').modal('hide');
+  });
+}
+
+function projectsDelete(e){
+  e.preventDefault();
+  $.ajax({
+    url: `${API}/users/${$(this).attr('data-user')}/projects/${$(this).attr('data-project')}`,
+    type: 'delete'
+  }).done(() => {
+    usersShow.bind(this)(e);
   });
 }
 
@@ -337,12 +286,68 @@ function projectsUpdate(e){
   });
 }
 
-function projectsDelete(e){
-  e.preventDefault();
-  $.ajax({
-    url: `${API}/users/${$(this).attr('data-user')}/projects/${$(this).attr('data-project')}`,
-    type: 'delete'
-  }).done(() => {
-    usersShow.bind(this)(e);
+//////////reusable functions/////////////
+function addUser(selector, user){
+  $(selector).prepend(`
+    <div class="col-xs-12 col-sm-4">
+      <div class="thumbnail profile">
+        <a href="/users/${user._id}" class="usersShow">
+          <img src="${user.image}" style="max-height: 200px">
+        </a>
+        <div class="caption">
+          <h3 class="title">${user.name}</h3>
+          <p>${user.bio}</p>
+        </div>
+      </div>
+    </div>
+  `);
+}
+
+function addFullUser(user){
+  addUser('.users', user);
+  addUserInfo('.profile .caption', user);
+  addUserButtons('.profile', user);
+  $('.projects').append(`
+    <button type="button" class="btn btn-default projectsNew" data-id=${user._id}>New</button>
+    `);
+  $(user.projects).each((index, project) =>{
+    const userId = user._id;
+    addProject('.projects', userId, project, index);
   });
+}
+
+function addUserInfo(selector, user){
+  $(selector).append(`
+  <p id="userGithub">${user.github}</p>
+  <p id="userTwitter">${user.twitter}</p>
+  <p id="userWebsite">${user.website}</p>
+  `);
+}
+
+function addUserButtons(selector, user){
+  $(selector).append(`
+  <div class="btn-group" role="group" aria-label="...">
+  <button type="button" class="btn btn-default usersEdit" data-id=${user._id}>Edit</button>
+  <button type="button" class="btn btn-danger usersDelete" data-id=${user._id}>Delete</button>
+  </div>
+  </div>
+  <h1>projects</h1>
+  <div class="projects">
+  </div>
+  `);
+}
+
+function addProject(selector, userId, project, index){
+  $(selector).prepend(`
+    <div class="project" id="project${index}">
+    <h2 class="title">${project.title}</h2>
+    <p class="description">${project.description}</p>
+    <p class="github">${project.github}</p>
+    <p class="website">${project.website}</p>
+    <div class="btn-group" role="group" aria-label="...">
+    <button type="button" class="btn btn-default projectsEdit" data-project="${project._id}" data-user="${userId}" data-index="${index}">Edit</button>
+    <button type="button" class="btn btn-danger projectsDelete" data-project="${project._id}" data-user="${userId}" data-index="${index}" href="/users/${userId}">Delete</button>
+    </div>
+    </div>
+    `);
 }
