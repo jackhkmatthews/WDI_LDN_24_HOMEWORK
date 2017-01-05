5b38080d1ca340ae36005d9fcfcdafcfdbecd5ec
jackhkmatthews <jackhkmatthews@gmail.com>
2017-01-05 20:59:43 +0000

    blacklisting

--- jackhkmatthews/w05d04/authentication-api/models/user.js
+++ jackhkmatthews/w05d04/authentication-api/models/user.js
@@ -7,11 +7,13 @@ const userSchema = new mongoose.Schema({
   passwordHash: { type: String, required: true }
 });
 
+//to register
+
 //when registering, the post request will include a password field
 //this password field is not included in the Schema
 //normally is would be ignored
 //we will make mongoose listen for this field
-//save it in a virtual field
+//save it in a virtual field to be compared against passwordConfirmation
 //hash it and save it in the passwordHash field
 
 userSchema
@@ -19,27 +21,25 @@ userSchema
   .set(setPassword);
 
 //value = password field
-function setPassword(value){
-  this._password = value;
-  this.passwordHash = bcrypt.hashSync(value, bcrypt.genSaltSync(8));
+function setPassword(password){
+  this._password = password;
+  this.passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
 }
 
-//repeat the above for the passwordConfirmation field
-
-//will listen for 'passwordConfirmation' and set a value
+//will listen for 'passwordConfirmation' and set a value to be compared
+//to virtual plain test password
 userSchema
   .virtual('passwordConfirmation')
   .set(setPasswordConfirmation);
 
-//Q1: are we comparing hashes? If so why not compare the passwords?
 //Q2: why isNew? as is testing on when ever a password hash is present
-//Q3: what does .virtual actually do? what is the method? is it a method?
+//Q3: what do .virtual and .path actually do? what is the method? is it a method?
 function setPasswordConfirmation(value){
   this._passwordConfirmation = value;
 }
 
-//will also listen for path passwordHash and validate it against
-//hash of password confirmation
+//will listen for path passwordHash and validate plain password in virtual field
+//against passwordConfirmation also stored in virtual field
 userSchema
   .path('passwordHash')
   .validate(validatePasswordHash);
@@ -68,6 +68,13 @@ function validatePassword(password) {
   return bcrypt.compareSync(password, this.passwordHash);
 }
 
-
+userSchema.set('toJSON', {
+  transform: function(doc, ret) {  //doc = mongo document, ret = return info
+    delete ret.passwordHash;
+    delete ret.email;
+    delete ret.__v;
+    return ret;
+  }
+});
 
 module.exports = mongoose.model('User', userSchema);
