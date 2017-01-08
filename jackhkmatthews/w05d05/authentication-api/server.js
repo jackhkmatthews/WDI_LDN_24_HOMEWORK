f96fb189da230747fac3c78cf2a053470a80add4
jackhkmatthews <jackhkmatthews@gmail.com>
2017-01-08 15:24:24 +0000

    requests being authorised based on whether token is present in header or not

--- jackhkmatthews/w05d05/authentication-api/server.js
+++ jackhkmatthews/w05d05/authentication-api/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/config');
 const router = require('./config/routes');
+const expressJWT = require('express-jwt');
 // const User = require('./models/user');
 
 // User.collection.drop();
@@ -15,6 +16,26 @@ mongoose.connect(config.db, () => console.log(`connected to ${config.db}`));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+//need to check for token before allowing requests to the api
+//pass secret and specify urls to be ignored
+app.use('/api', expressJWT({ secret: config.secret})
+  .unless({
+    path: [
+      {url: '/api/users/login', method: ['POST']},
+      {url: '/api/users/register', method: ['POST']}
+    ]
+  }));
+
+//handling the errors
+app.use(jwtErrorHandler);
+
+function jwtErrorHandler(err, req, res, next){
+  if (err.name !== 'UnauthorizedError') return next();
+  return res.status(401).json({ message: 'Unauthorized request.' });
+}
+
 app.use('/api', router);
 
 app.listen(config.port, () => console.log(`express listening at port ${config.port}`));
